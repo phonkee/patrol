07d391ce83a083d2c75f08b78084562033346a25
phonkee <phonkee@phonkee.eu>
2015-05-27 00:56:32 +0200

    Update of PrettyPrint

--- apitest/helpers.go
+++ apitest/helpers.go
@@ -12,28 +12,28 @@ if data is string - PrettyPrint first unmarshals this to map[string]interface{}
 and then pretty prints it. This feature is e.g. for http responses
 
 */
-func PrettyPrint(format string, data interface{}) error {
+func PrettyPrint(format string, data interface{}) (err error) {
 	format = format + "\n"
 
 	switch data.(type) {
 	case string:
 		m := map[string]interface{}{}
-		if err := json.Unmarshal([]byte(data.(string)), &m); err != nil {
+		if err = json.Unmarshal([]byte(data.(string)), &m); err != nil {
 			return err
 		}
 		data = m
 	case []byte:
 		m := map[string]interface{}{}
-		if err := json.Unmarshal(data.([]byte), &m); err != nil {
+		if err = json.Unmarshal(data.([]byte), &m); err != nil {
 			return err
 		}
 		data = m
 	}
 
-	body, err := json.MarshalIndent(data, "", "    ")
-	if err != nil {
+	var body []byte
+	if body, err = json.MarshalIndent(data, "", "    "); err != nil {
 		return err
 	}
-	fmt.Printf("%+v\n", string(body))
+	fmt.Printf(format, string(body))
 	return nil
 }
